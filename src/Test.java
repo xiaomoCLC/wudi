52f524493455d8471c48f4cf71527c26f71afaad
xiaomo <xiaomo_CLC@163.com>
2021-06-08 18:20:47 +0800

    初始化

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Author:xiaomo
+ * @Classname Test
+ * @Date 2021/6/8 18:19
+ * @Created by 小默。
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
